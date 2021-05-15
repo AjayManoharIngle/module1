5deae98a9a731aa4dd127bbab3037646f01df5ae
apnafolio <apnafolio@gmail.com>
2021-05-16 00:13:09 +0530

    100

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ class Main {
   public static void main(String[] args) {
     
     System.out.println("Enter two numbers");
-    int first = 10;
-    int second = 20;
+    int first = 40;
+    int second = 60;
     
     System.out.println(first + " " + second);
 
